4ca31fc81079e66ca60bc210e41caf102af157e6
pramod-wdo <pramodhc007@gmail.com>
2022-09-19 13:05:20 +0530

    adds webhooks controller to get response from stripe

---
+++ app/controllers/webhooks_controller.rb
@@ -0,0 +1,30 @@
+class WebhooksController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    payload = request.body.read
+    sig_header = request.env['HTTP_STRIPE_SIGNATURE']
+    event = nil
+    begin
+      event = Stripe::Webhook.construct_event(
+        payload, sig_header, Rails.application.credentials[:stripe][:webhook]
+      )
+    rescue JSON::ParseError => e
+      status 400
+      return
+    rescue Stripe::SignatureVerificationError => e
+      puts "Signature error"
+      p e
+      return
+    end
+
+    case event.type
+    when 'checkout.session.completed'
+      session = event.data.object
+      @product = Product.find_by(price: session.amount_received)
+      @product.increment!(:sales_count)
+    end
+
+    render json: {message: 'success'}
+  end
+end
